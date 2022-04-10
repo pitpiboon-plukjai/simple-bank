3e6f8bd9011671eade659f21115f55f05b1b0945
Pitpiboon (Earth) Plukjai <pitpiboon.p@aware.co.th>
2022-04-10 13:13:14 +0700

    first commit

---
+++ src/Web3ReactConnectionComponent.js
@@ -0,0 +1,105 @@
+import { useWeb3React } from '@web3-react/core';
+import { injected, walletconnect, resetWalletConnector, walletlink } from './Helpers/connectors';
+import { getContract } from './Helpers/contract';
+import React from 'react';
+
+const Web3ReactConnectionComponent = () => {
+	//connector, library, chainId, account, activate, deactivate
+	const web3reactContext = useWeb3React(); 
+	//web3react
+	const writeToContractUsingWeb3React = async () => {
+		try {
+			const randomNumber = Math.floor(Math.random() * 100);
+			const myContract = getContract(web3reactContext.library, web3reactContext.account);
+			const overrides = {
+				gasLimit: 230000
+			};
+			const response = await myContract.store(randomNumber, overrides);
+			console.log(response);
+			alert('write ' + randomNumber);
+		} catch (ex) {
+			console.log(ex);
+			alert(ex);
+		}
+	};
+
+	const disconnectMetamaskSimple = () => {
+		try {
+			web3reactContext.deactivate();
+		} catch (ex) {
+			console.log(ex);
+		}
+	};
+
+	//web3react context
+	const checkInfoSimple = async () => {
+		try {
+			console.log('web3reactContext');
+			console.log(web3reactContext);
+		} catch (ex) {
+			console.log(ex);
+		}
+	};
+
+	//web3react metamask
+	const connectMetamaskSimple = async () => {
+		try {
+			await web3reactContext.activate(injected);
+		} catch (ex) {
+			console.log(ex);
+		}
+	};
+
+	//web3react walletconnect
+	const connectWalletConnectSimple = async () => {
+		try {
+			resetWalletConnector(walletconnect);
+			await web3reactContext.activate(walletconnect);
+		} catch (ex) {
+			console.log(ex);
+		}
+	};
+
+	//web3react coinbase
+	const connectCoinbaseSimple = async () => {
+		try {
+			await web3reactContext.activate(walletlink);
+		} catch (ex) {
+			console.log(ex);
+		}
+	};
+
+	return (
+		<div className="flex flex-col space-y-7 items-start pt-10 w-1/2 border-2 border-yellow-300">
+			<h2>Simple-bank Control</h2>
+			{web3reactContext.account ? <p>{web3reactContext.account}</p> : <p>Not connected</p>}
+			<div className="flex space-x-3">
+				<button
+					className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+					onClick={writeToContractUsingWeb3React}
+				>
+					Write To Contract Via Simple-bank
+				</button>
+			</div>
+			<div className="flex space-x-3">
+				<button
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+					onClick={connectMetamaskSimple}
+				>
+					Connect Metamask Via Simple-bank
+				</button>
+			</div>
+			<div className="flex space-x-3">
+			<button
+					className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+					onClick={disconnectMetamaskSimple}
+				>
+					Disconnect Simple-bank
+				</button>
+			</div>
+			<div className="flex space-x-3">
+			</div>
+		</div>
+	);
+};
+export default Web3ReactConnectionComponent;
---
+++ truffle/contracts/NFT.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.0;
+
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721 {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenId;
+
+    constructor() ERC721("TestNFT", "TEST") {}
+
+    function mint(address owner) public {
+        _tokenId.increment();
+
+        unit256 newTokenID = _tokenId.current();
+        _mint(owner, newTokenID);
+    }
+}
